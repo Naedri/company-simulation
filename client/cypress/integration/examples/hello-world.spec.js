dd2d2dd055899819d0ae3de8990ffb6d9872e056
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2021-05-17 15:26:01 +0200

    add cypress

---
+++ client/cypress/integration/examples/hello-world.spec.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+describe('The Home Page', () => {
+    it('successfully loads', () => {
+        cy.visit('http://localhost:3000') // change URL to match your dev URL
+    })
+})
