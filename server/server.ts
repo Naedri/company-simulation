28127f4ad36d9f1424d16f899b8f878c3d9bf016
Adrien Jallais <adrien.jallais@protonmail.com>
2021-05-26 16:50:14 +0200

    [feat] middleware to check cookie

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,27 @@
 import express from "express";
 import router from "./router";
+import config from './services/user/config';
+
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+const jwt = require('express-jwt');
 
 const app = express();
 const PORT = "3000";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cookieParser());
+app.use(
+    jwt({
+        secret: config.token.secret,
+        algorithms: ['HS256'],
+        getToken: (req: any) => req.cookies.token
+    }).unless({path: ['/users/login']})
+);
 app.use("/", router);
 
-app.use(function(req, res, next){
+app.use(function(req, res){
     res.json({
         error:{
             status: 404,
