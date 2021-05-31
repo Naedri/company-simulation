0a297f670bec907bbc83fc3925f8f70b9565be25
Louis Muzellec <louis.muzellec@imt-atlantique.net>
2021-05-31 15:50:49 +0200

    swagger router in his own file

--- server/router.ts
+++ server/router.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import swaggerUI from "swagger-ui-express";
-import * as swaggerDocument from "./swagger.json";
 import {router as usersRouter, USERS_ROUTES_BASE_PATH} from "./routes/usersRoutes";
+import {router as swaggerRouter, SWAGGER_ROUTES_BASE_PATH} from "./routes/swaggerRoutes";
 
 const router = express.Router();
 
 router.use(USERS_ROUTES_BASE_PATH, usersRouter);
-router.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+router.use(SWAGGER_ROUTES_BASE_PATH, swaggerRouter);
 
 export default router;
