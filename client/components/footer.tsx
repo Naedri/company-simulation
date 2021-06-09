7b5abcd713839164e3533955e3e08b5506ecc719
Adrien Jallais <adrien.jallais@protonmail.com>
2021-06-09 11:37:37 +0200

    [feat] header

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import Error from "next/error";
 import { useRouter } from "next/router";
 import { getUserInfo, register } from "../utils/rest/auth";
+import { FaKissWinkHeart } from 'react-icons/fa';
+
+import styles from '../styles/Footer.module.css';
 
 export default function Form() {
     const router = useRouter();
@@ -10,16 +13,10 @@ export default function Form() {
     const [logError, setError] = useState<Error | null>(null);
     const [logSuccess, setLogSuccess] = useState(false);
 
-    const footerStyle = {
-        backgroundColor: "red",
-        color: "white",
-        width: "100%",
-        height: "60px"
-    };
-
     return (
-        <div className={"Footer"} style={footerStyle}>
-            FOOTER
+        <div className={"footer"}>
+            <div className={styles.draft}></div>
+            Made with <FaKissWinkHeart /> by IMT Nantes - FIL
         </div>
     );
 }
--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, ReactComponentElement } from "react";
 import Link from "next/link";
 import Error from "next/error";
 import { useRouter } from "next/router";
 import { getUserInfo, register } from "../utils/rest/auth";
+import styles from '../styles/Header.module.css';
+import { GiFactory } from 'react-icons/gi';
+
+// import { ReactComponentElement as logoIMT } from '../utils/icons/IMT_LOGO.svg';
 
 export default function Form() {
     const router = useRouter();
@@ -10,16 +14,29 @@ export default function Form() {
     const [logError, setError] = useState<Error | null>(null);
     const [logSuccess, setLogSuccess] = useState(false);
 
-    const headerStyle = {
-        backgroundColor: "blue",
-        color: "white",
-        width: "100%",
-        height: "50px"
-    };
+
+
+/*
+    <Link href="/dashboard"><li className={`${styles.menu__item} ${this.props.dashboardOpen ? styles.menu__link__active : ''}`}>Dashboard</a></li></Link>
+    <Link href="/login"><li className={`${styles.menu__item} ${this.props.loginOpen ? styles.menu__link__active : ''}`}>Login</a></li></Link>
+    <Link href="/signin"><li className={`${styles.menu__item} ${this.props.signinOpen ? styles.menu__link__active : ''}`}>Signin</a></li></Link>
+*/
 
     return (
-        <div className={"Header"} style={headerStyle}>
-            HEADER
+        <div className={styles.container}>
+            <header className={`${styles.menu} ${styles.menu__border__bottom}`}>
+                <div className={styles.menu__title}>
+                    <div className={styles.menu__logo}>
+                        <GiFactory />
+                    </div>
+                    <span>Web Simulation</span>
+
+                </div>
+                <ul className={styles.menu__navigation}>
+                    <Link href="/dashboard"><li className={styles.menu__item}><a className={styles.menu__link}>Dashboard</a></li></Link>
+                    <Link href="/login"><li className={styles.menu__item}><a className={styles.menu__link}>Logout</a></li></Link>
+                </ul>
+            </header>
         </div>
     );
 }
