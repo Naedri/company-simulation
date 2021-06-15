5b1614fc17297a0913351b8d5f459153b63d8c6a
Adrien Jallais <adrien.jallais@protonmail.com>
2021-06-15 09:56:33 +0200

    [feat] footer link to github repo

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -1,13 +1,15 @@
-
+import Link from "next/link";
 import { AiFillHeart } from 'react-icons/ai';
 import styles from '../styles/Footer.module.css';
 
 export default function Form() {
     return (
         <div className={styles.footer__container}>
-            <div className={styles.footer__element}>
-            Made with  <AiFillHeart />  by students from IMT Nantes - FIL promotion
-            </div>
+            <Link href="https://github.com/Naedri/company-simulation/">
+                <div className={styles.footer__element}>
+                    Made with  <AiFillHeart />  by students from IMT Nantes - FIL promotion
+                </div>
+            </Link>
         </div>
     );
 }
