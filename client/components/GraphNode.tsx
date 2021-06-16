772659238fab6fe3cfbeb70aa4b72ad300fd5aea
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2021-06-16 16:58:46 +0200

    add popover on graph node

--- client/components/GraphNode.tsx
+++ client/components/GraphNode.tsx
@@ -1,11 +1,19 @@
-import { FC } from "react";
-import { Node } from "../librairies/@types/DiagramSchema";
-import { IComponent } from "../utils/model/IComponent";
+import {FC, useState} from "react";
+import {Node} from "../librairies/@types/DiagramSchema";
+import {IComponent} from "../utils/model/IComponent";
 import styles from "../styles/GraphNode.module.css";
-import { setSelectedNode, useGraphContext } from "../contexts/GraphContext";
+import {setSelectedNode, useGraphContext} from "../contexts/GraphContext";
+import {usePopper} from "react-popper";
 
 const GraphNode: FC<Node<IComponent>> = (props) => {
-    const { selectedNode, setGraphState } = useGraphContext();
+    const {selectedNode, setGraphState} = useGraphContext();
+    const [visible, setVisibility] = useState(false);
+    const [referenceElement, setReferenceElement] = useState(null);
+    const [popperElement, setPopperElement] = useState(null);
+    const [arrowElement, setArrowElement] = useState(null);
+    const {styles: s, attributes} = usePopper(referenceElement, popperElement, {
+        modifiers: [{name: 'arrow', options: {element: arrowElement}}],
+    });
 
     const handleClick = () => {
         setSelectedNode(props.data, setGraphState);
@@ -20,12 +28,28 @@ const GraphNode: FC<Node<IComponent>> = (props) => {
         backgroundColor: "" + props.data.fields.color,
     };
     return (
-        <div className={styles.component} style={styleNode} onMouseDown={handleClick}>
-            <div className={styles.header}>
-                {props.id}
+        <>
+            <div ref={setReferenceElement}
+                 onPointerOver={() => setVisibility(true)}
+                 onPointerOut={() => setVisibility(false)}
+                 className={styles.component} style={styleNode} onMouseDown={handleClick}>
+                <div className={styles.header}>
+                    {props.id}
+                </div>
             </div>
-        </div>
+            <div ref={setPopperElement}
+                 style={{...s.popper,
+                     visibility: `${visible ? "visible" : "hidden"}`,
+                     backgroundColor: "white",
+                     zIndex: 1000,
+                     padding: "0.5rem",
+                     borderRadius: "6px",
+                 }
+                 } {...attributes.popper}>
+                {props.data.type}
+                <div ref={setArrowElement} style={s.arrow}/>
+            </div>
+        </>
     );
-};
-
+}
 export default GraphNode;
